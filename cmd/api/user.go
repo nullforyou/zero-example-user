bef7e1f1078b62f5c923f06a33ed29dcb2a3c446
jiang7552261 <jiang7552261@163.com>
2023-08-25 17:40:30 +0800

    JWT验证失败自定义处理

--- cmd/api/user.go
+++ cmd/api/user.go
@@ -6,6 +6,9 @@ import (
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
+	"go-zero-base/utils/response"
+	"go-zero-base/utils/xerr"
+	"net/http"
 	"user/cmd/api/internal/config"
 	"user/cmd/api/internal/handler"
 	"user/cmd/api/internal/svc"
@@ -21,7 +24,10 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c, conf.UseEnv())
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(func(w http.ResponseWriter, r *http.Request, err error) {
+		//JWT验证失败自定义处理
+		response.Response(r, w, nil, xerr.NewBusinessError(xerr.SetCode(xerr.ErrorTokenExpire)))
+	}))
 	defer server.Stop()
 
 	serverCtx := svc.NewServiceContext(c)
